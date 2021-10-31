ff91fc544d0d72123c0470a68d1207d0ed9366b6
aki360P <34680805+aki360P@users.noreply.github.com>
2021-10-31 14:04:22 +0900

    routing update for Rails6.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  #resources :projects do
-  #  resources :remp_boards
-  #  match 'settings/remp_setting/:action', :controller => 'remp_settings', :via => [:get, :post, :patch, :put]
-  #end
+  #add /projects/:id
+  resources :projects do
+    #add get /projects/:id/remp_boards using controller 'remp_boards' action 'index' 
+    resources :remp_boards, only: [:index]
 
-  match 'projects/:project_id/remp_boards/:action', :controller => 'remp_boards', :via => [:get]
-  match 'projects/:project_id/settings/remp_setting/:action', :controller => 'remp_settings', :via => [:get, :post, :patch, :put]
+    #add patch /projects/:id/settings/remp_setting using controller 'remp_settings' action 'edit'
+    patch :remp_setting, path: '/settings/remp_setting', controller: 'remp_settings', action: :edit
+  end
+
+  #old Using a dynamic :action segment in a route is deprecated and will be removed in Rails 6.0.
+  #match 'projects/:project_id/remp_boards/:action', :controller => 'remp_boards', :via => [:get]
+  #match 'projects/:project_id/settings/remp_setting/:action', :controller => 'remp_settings', :via => [:get, :post, :patch, :put]
 end
--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_empty_plugin do
   name 'Redmine empty plugin'
   author 'Akinori Iwasaki'
   description 'Sample plugin for beginner developers'
-  version '0.1.0'
+  version '0.2.0'
   url 'https://github.com/aki360P/redmine_empty_plugin'
   
   project_module :redmine_empty_plugin do
